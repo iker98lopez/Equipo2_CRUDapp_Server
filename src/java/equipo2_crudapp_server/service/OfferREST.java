9d53592304a3034dc7ade0439ff643d41770cd8a
iker lopez carrillo <iker98lopez@gmail.com>
2020-02-10 00:00:58 +0100

    Fixed offerREST

--- src/java/equipo2_crudapp_server/service/OfferREST.java
+++ src/java/equipo2_crudapp_server/service/OfferREST.java
@@ -1,6 +1,8 @@
 package equipo2_crudapp_server.service;
 
 import equipo2_crudapp_server.entities.Offer;
+import equipo2_crudapp_server.entities.Software;
+import equipo2_crudapp_server.entities.User;
 import java.util.Set;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -28,12 +30,24 @@ public class OfferREST {
      * Logger to show error messages and exceptions.
      */
     private static final Logger LOGGER = Logger.getLogger("equipo2_crudapp_server.services.OfferREST");
-    
+
     /**
      * Enterprise Java Beans for the entity Offer.
      */
     @EJB
     private EJBOfferInterface ejbOffer;
+
+    /**
+     * Enterprise Java Beans for the entity Offer.
+     */
+    @EJB
+    private EJBSoftwareInterface ejbSoftware;
+
+    /**
+     * Enterprise Java Beans for the entity User.
+     */
+    @EJB
+    private EJBUserInterface ejbUser;
     
     /**
      * Creates a new offer in the database.
@@ -44,11 +58,19 @@ public class OfferREST {
     @Consumes({MediaType.APPLICATION_XML})
     public void createOffer(Offer offer) {
         try {
+            Software software = ejbSoftware.findAllSoftwares().stream().filter(sw -> sw.getOffers() != null
+                && sw.getOffers().contains(offer))
+                .findFirst().get();
+            offer.setSoftware(software);
+            User user = ejbUser.findAllUsers().stream().filter(usr -> usr.getOffers() != null 
+                    && usr.getOffers().contains(offer))
+                    .findFirst().get();
+            offer.setUser(user);
             ejbOffer.createOffer(offer);
         } catch (EJBException exception) {
             LOGGER.warning("There was an error creating a new offer. " + exception.getMessage());
         } catch (Exception exception) {
-            LOGGER.warning ("There was an unexpected exception. " + exception.getClass() + " " + exception.getMessage());
+            LOGGER.warning("There was an unexpected exception. " + exception.getClass() + " " + exception.getMessage());
         }
     }
 
@@ -62,18 +84,26 @@ public class OfferREST {
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_XML})
     public void editOffer(@PathParam("id") Integer id, Offer offer) {
+        Software software = ejbSoftware.findAllSoftwares().stream().filter(sw -> sw.getOffers() != null
+                && sw.getOffers().contains(offer))
+                .findFirst().get();
+        offer.setSoftware(software);
+        User user = ejbUser.findAllUsers().stream().filter(usr -> usr.getOffers() != null 
+                && usr.getOffers().contains(offer))
+                .findFirst().get();
+        offer.setUser(user);
         ejbOffer.modifyOffer(offer);
     }
 
     /**
      * Deletes an offer from the database.
      *
-     * @param offer offer to delete.
+     * @param offerId ID of the offer to delete.
      */
     @DELETE
-    @Path("{offer}")
-    public void removeOffer(@PathParam("offer") Offer offer) {
-        ejbOffer.deleteOffer(offer);
+    @Path("{id}")
+    public void removeOffer(@PathParam("id") int offerId) {
+        ejbOffer.deleteOffer(ejbOffer.findOffer(offerId));
     }
 
     /**
